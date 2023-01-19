447821abfe63dc315da02386ef78d1ca48fdd3cc
ripalkumar <patelripalacc@gmail.com>
2023-01-19 15:40:30 -0500

    finish

--- app.js
+++ app.js
@@ -15,10 +15,28 @@ function getList() {
   });
 }
 
+const selectP = document.querySelector("#error");
+const selectUl = document.querySelector("#list");
+
 // TODO: Handle the resolved or rejected states of the promise
+
+// getList()
+//   .then((value) => console.log(value))
+//   .catch((error) => console.error(error));
 
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
-
-// TODO: If the promise rejects with the failure object
-// Display the failure message in the paragraph element with id="error" (check index.html file)
+getList()
+  .then((value) => {
+    value.forEach((element) => {
+      let li = document.createElement("li");
+      li.textContent = element;
+      selectUl.appendChild(li);
+    });
+  })
+  // TODO: If the promise rejects with the failure object
+  // Display the failure message in the paragraph element with id="error" (check index.html file)
+  .catch((error) => {
+    console.error(error);
+    selectP.textContent = error.message;
+  });  
